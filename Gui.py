df5ef24c3c8a5a40356d158a91c5296fb5ee7fcc
himaji <himaji.mac@gmail.com>
2021-08-09 10:30:56 +0900

    adjustment gui
    個数入力と値の取得の追加

--- Gui.py
+++ Gui.py
@@ -9,31 +9,40 @@ class Gui():
 
 
         def btn_click():
-            array = []
+            menu_array = []
+            quantity_array = []
             if txt1.get() != "" :
-                array.append(txt1.get())
+                menu_array.append(txt1.get())
+                quantity_array.append(int(quantity1.get()))
             if txt2.get() != "" :
-                array.append(txt2.get())
+                menu_array.append(txt2.get())
+                quantity_array.append(int(quantity2.get()))
             if txt3.get() != "" :
-                array.append(txt3.get())
+                menu_array.append(txt3.get())
+                quantity_array.append(int(quantity3.get()))
             if txt4.get() != "" :
-                array.append(txt4.get())
+                menu_array.append(txt4.get())
+                quantity_array.append(int(quantity4.get()))
             if txt5.get() != "" :
-                array.append(txt5.get())
+                menu_array.append(txt5.get())
+                quantity_array.append(int(quantity5.get()))
             if txt6.get() != "" :
-                array.append(txt6.get())
+                menu_array.append(txt6.get())
+                quantity_array.append(int(quantity6.get()))
             if txt7.get() != "" :
-                array.append(txt7.get())
+                menu_array.append(txt7.get())
+                quantity_array.append(int(quantity7.get()))
             if txt8.get() != "" :
-                array.append(txt8.get())
+                menu_array.append(txt8.get())
+                quantity_array.append(int(quantity8.get()))
             if txt9.get() != "" :
-                array.append(txt9.get())
+                menu_array.append(txt9.get())
+                quantity_array.append(int(quantity9.get()))
             if txt10.get() != "" :
-                array.append(txt10.get())
-
-            print(array)
-            pptx_auto_gen.auto_gen(array)
-            xlsx_auto_gen.auto_gen(array)
+                menu_array.append(txt10.get())
+                quantity_array.append(int(quantity10.get()))
+            pptx_auto_gen.auto_gen(menu_array)
+            xlsx_auto_gen.auto_gen(menu_array, quantity_array)
 
 
 
@@ -42,7 +51,7 @@ class Gui():
         self.main.geometry("350x700")
 
         # タイトルラベル
-        lbl_title = tkinter.Label(text='メニューを入力して作成ボタンを押してください')
+        lbl_title = tkinter.Label(text='メニューと個数を入力して作成ボタンを押してください')
         lbl_title.place(x=5, y=20)
 
         # ラベル
@@ -51,6 +60,8 @@ class Gui():
         # テキストボックス
         txt1 = tkinter.Entry(width=30)
         txt1.place(x=30, y=70)
+        quantity1 = tkinter.Entry(width=2)
+        quantity1.place(x=310, y=70)
 
         # ラベル
         lbl2 = tkinter.Label(text='2')
@@ -58,6 +69,8 @@ class Gui():
         # テキストボックス
         txt2 = tkinter.Entry(width=30)
         txt2.place(x=30, y=100)
+        quantity2 = tkinter.Entry(width=2)
+        quantity2.place(x=310, y=100)
 
         # ラベル
         lbl3 = tkinter.Label(text='3')
@@ -65,6 +78,8 @@ class Gui():
         # テキストボックス
         txt3 = tkinter.Entry(width=30)
         txt3.place(x=30, y=130)
+        quantity3 = tkinter.Entry(width=2)
+        quantity3.place(x=310, y=130)
 
         # ラベル
         lbl4 = tkinter.Label(text='4')
@@ -72,6 +87,8 @@ class Gui():
         # テキストボックス
         txt4 = tkinter.Entry(width=30)
         txt4.place(x=30, y=160)
+        quantity4 = tkinter.Entry(width=2)
+        quantity4.place(x=310, y=160)
 
         # ラベル
         lbl5 = tkinter.Label(text='5')
@@ -79,6 +96,8 @@ class Gui():
         # テキストボックス
         txt5 = tkinter.Entry(width=30)
         txt5.place(x=30, y=190)
+        quantity5 = tkinter.Entry(width=2)
+        quantity5.place(x=310, y=190)
 
         # ラベル
         lbl6 = tkinter.Label(text='6')
@@ -86,6 +105,8 @@ class Gui():
         # テキストボックス
         txt6 = tkinter.Entry(width=30)
         txt6.place(x=30, y=220)
+        quantity6 = tkinter.Entry(width=2)
+        quantity6.place(x=310, y=220)
 
         # ラベル
         lbl7 = tkinter.Label(text='7')
@@ -93,6 +114,8 @@ class Gui():
         # テキストボックス
         txt7 = tkinter.Entry(width=30)
         txt7.place(x=30, y=250)
+        quantity7 = tkinter.Entry(width=2)
+        quantity7.place(x=310, y=250)
 
         # ラベル
         lbl8 = tkinter.Label(text='8')
@@ -100,6 +123,8 @@ class Gui():
         # テキストボックス
         txt8 = tkinter.Entry(width=30)
         txt8.place(x=30, y=280)
+        quantity8 = tkinter.Entry(width=2)
+        quantity8.place(x=310, y=280)
 
         # ラベル
         lbl9 = tkinter.Label(text='9')
@@ -107,6 +132,8 @@ class Gui():
         # テキストボックス
         txt9 = tkinter.Entry(width=30)
         txt9.place(x=30, y=310)
+        quantity9 = tkinter.Entry(width=2)
+        quantity9.place(x=310, y=310)
 
         # ラベル
         lbl10 = tkinter.Label(text='10')
@@ -114,12 +141,12 @@ class Gui():
         # テキストボックス
         txt10 = tkinter.Entry(width=30)
         txt10.place(x=30, y=340)
-
-        array = []
+        quantity10 = tkinter.Entry(width=2)
+        quantity10.place(x=310, y=340)
 
         # 作成ボタン
-        btn1 = tkinter.Button(self.main, text='送信', width=15,height=3,command = btn_click)
-        btn1.place(x=60,y=370)
+        btn1 = tkinter.Button(self.main, text='作成', width=15,height=3,command = btn_click)
+        btn1.place(x=100,y=370)
 
         self.main.mainloop()
 
