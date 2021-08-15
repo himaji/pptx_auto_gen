0f611c35318b12540fa1692bd5232b3c84ddf357
himaji <himaji.mac@gmail.com>
2021-08-15 14:18:50 +0900

    sales quantity feature
    販売個数の記録機能
    販売単価ごとに記録するため、弁当名から販売単価を取得し、そのカラムに書き込む機能実装予定

--- Gui2.py
+++ Gui2.py
@@ -3,6 +3,8 @@ import tkinter.ttk as ttk
 import sells_report_auto_gen
 import pandas as pd
 import datetime as dt
+from functools import partial
+import openpyxl
 
 class MainWindow(tk.Frame):
     def __init__(self, master=None, parent=None):
@@ -17,27 +19,26 @@ class MainWindow(tk.Frame):
         self.create_widgets()
 
     def set_data(self):
-        self.data = pd.read_excel("../output/sales_management.xlsx")
-        df = pd.read_excel("../output/sales_management.xlsx")
-        flg = df["日付"].astype("str").str.isdigit()
-        from_serial = pd.to_timedelta(df.loc[flg, "日付"].astype("int"), unit="D") + pd.to_datetime("1900/1/1")
-        from_string = pd.to_datetime(df.loc[~flg, "日付"])
-        df["日付"] = pd.concat([from_serial, from_string])
-        self.colname_list = ["日付", "弁当名等", "搬入個数", "販売個数"]  # 結果に表示させる列名
-        self.width_list = [100, 200]
+        self.data = pd.read_excel("../output/sales_management.xlsx" ,header=0,index_col=None)
+        self.colname_list = ["日付", "弁当名等", "搬入個数"]  # 結果に表示させる列名
+        self.width_list = [100, 200, 50]
         self.search_col = "日付"  # 検索キーワードの入力されている列名
 
     def create_widgets(self):
         self.pw_main = ttk.PanedWindow(self.master, orient="vertical")
         self.pw_main.pack(expand=True, fill=tk.BOTH, side="left")
-        self.pw_top = ttk.PanedWindow(self.pw_main, orient="horizontal", height=25)
+        self.pw_top = ttk.PanedWindow(self.pw_main, orient="horizontal", height=30)
         self.pw_main.add(self.pw_top)
         self.pw_bottom = ttk.PanedWindow(self.pw_main, orient="vertical")
         self.pw_main.add(self.pw_bottom)
-        self.creat_input_frame(self.pw_top)
-        self.create_tree(self.pw_bottom)
+        self.pw_bottom2 = ttk.PanedWindow(self.pw_main, orient="vertical")
+        self.pw_main.add(self.pw_bottom2)
 
-    def creat_input_frame(self, parent):
+        self.create_input_frame(self.pw_top)
+        self.create_tree(self.pw_bottom)
+        self.create_update_frame(self.pw_bottom2)
+
+    def create_input_frame(self, parent):
         fm_input = ttk.Frame(parent, )
         parent.add(fm_input)
         lbl_keyword = ttk.Label(fm_input, text="キーワード", width=7)
@@ -62,9 +63,99 @@ class MainWindow(tk.Frame):
 
     def search(self, event=None):
         keyword = dt.datetime.strptime(self.keyword.get(), '%Y-%m-%d')
-        print(keyword)
         result = self.data[self.data['日付'] == keyword]
         self.update_tree_by_search_result(result)
+
+
+    def create_update_frame(self, parent):
+        fm_update = ttk.Frame(parent,)
+        parent.add(fm_update)
+        lbl_update1 = ttk.Label(fm_update, text="1")
+        lbl_update1.grid(row=1, column=1, padx=2, pady=2)
+        self.ent_update1 = ttk.Entry(fm_update, justify="left")
+        self.ent_update1.grid(row=1, column=2, padx=2, pady=2)
+        lbl_update2 = ttk.Label(fm_update, text="2")
+        lbl_update2.grid(row=2, column=1, padx=2, pady=2)
+        self.ent_update2 = ttk.Entry(fm_update, justify="left")
+        self.ent_update2.grid(row=2, column=2, padx=2, pady=2)
+        lbl_update3 = ttk.Label(fm_update, text="3")
+        lbl_update3.grid(row=3, column=1, padx=2, pady=2)
+        self.ent_update3 = ttk.Entry(fm_update, justify="left")
+        self.ent_update3.grid(row=3, column=2, padx=2, pady=2)
+        lbl_update4 = ttk.Label(fm_update, text="4")
+        lbl_update4.grid(row=4, column=1, padx=2, pady=2)
+        self.ent_update4 = ttk.Entry(fm_update, justify="left")
+        self.ent_update4.grid(row=4, column=2, padx=2, pady=2)
+        lbl_update5 = ttk.Label(fm_update, text="5")
+        lbl_update5.grid(row=5, column=1, padx=2, pady=2)
+        self.ent_update5 = ttk.Entry(fm_update, justify="left")
+        self.ent_update5.grid(row=5, column=2, padx=2, pady=2)
+        lbl_update6 = ttk.Label(fm_update, text="6")
+        lbl_update6.grid(row=6, column=1, padx=2, pady=2)
+        self.ent_update6 = ttk.Entry(fm_update, justify="left")
+        self.ent_update6.grid(row=6, column=2, padx=2, pady=2)
+        lbl_update7 = ttk.Label(fm_update, text="7")
+        lbl_update7.grid(row=7, column=1, padx=2, pady=2)
+        self.ent_update7 = ttk.Entry(fm_update, justify="left")
+        self.ent_update7.grid(row=7, column=2, padx=2, pady=2)
+        lbl_update8 = ttk.Label(fm_update, text="8")
+        lbl_update8.grid(row=8, column=1, padx=2, pady=2)
+        self.ent_update8 = ttk.Entry(fm_update, justify="left")
+        self.ent_update8.grid(row=8, column=2, padx=2, pady=2)
+        lbl_update9 = ttk.Label(fm_update, text="9")
+        lbl_update9.grid(row=9, column=1, padx=2, pady=2)
+        self.ent_update9 = ttk.Entry(fm_update, justify="left")
+        self.ent_update9.grid(row=9, column=2, padx=2, pady=2)
+        lbl_update10 = ttk.Label(fm_update, text="10")
+        lbl_update10.grid(row=10, column=1, padx=2, pady=2)
+        self.ent_update10 = ttk.Entry(fm_update, justify="left")
+        self.ent_update10.grid(row=10, column=2, padx=2, pady=2)
+
+        btn_current = ttk.Button(fm_update, text="更新", command=lambda: self.update_result())
+        btn_current.grid(row=11, column=1, padx=2, pady=2)
+
+    def update_result(self):
+        if self.keyword.get() != "" :
+            print("update_result")
+            self.ent_index = []
+            if self.ent_update1.get() != "":
+                self.ent_index.append(self.ent_update1.get())
+            if self.ent_update2.get() != "":
+                self.ent_index.append(self.ent_update2.get())
+            if self.ent_update3.get() != "":
+                self.ent_index.append(self.ent_update3.get())
+            if self.ent_update4.get() != "":
+                self.ent_index.append(self.ent_update4.get())
+            if self.ent_update5.get() != "":
+                self.ent_index.append(self.ent_update5.get())
+            if self.ent_update6.get() != "":
+                self.ent_index.append(self.ent_update6.get())
+            if self.ent_update7.get() != "":
+                self.ent_index.append(self.ent_update7.get())
+            if self.ent_update8.get() != "":
+                self.ent_index.append(self.ent_update8.get())
+            if self.ent_update9.get() != "":
+                self.ent_index.append(self.ent_update9.get())
+            if self.ent_update10.get() != "":
+                self.ent_index.append(self.ent_update10.get())
+            keyword = dt.datetime.strptime(self.keyword.get(), '%Y-%m-%d')
+            data_index_use = self.data[self.data['日付'] == keyword]
+            for (index, num) in zip(data_index_use.index, self.ent_index):
+                self.data.at[index, "販売個数"] = num
+            
+            workbook = openpyxl.load_workbook("../output/sales_management.xlsx")
+            #ワークシート指定
+            sheet = workbook['販売個数']
+
+            sales_quantity = 18 # 販売個数のカラム番号
+            # for (date, menu_name, quantity) in zip(keyword, data_index_use['弁当名等'], self.ent_index): 
+            #     if sheet.cell(row=)
+
+            for (index, num) in zip(data_index_use.index, self.ent_index):
+                sheet.cell(index + 2, sales_quantity, value=int(num))
+            workbook.save('../output/sales_management.xlsx')
+
+
 
     def update_tree_by_search_result(self, result):
         self.tree.delete(*self.tree.get_children())
